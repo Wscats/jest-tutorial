228931523e4ebf27ee11505373798252413d2447
enoyao <enoyao@tencent.com>
2021-07-21 11:36:33 +0800

    Update jest.js

--- jest.js
+++ jest.js
@@ -3,35 +3,40 @@
  * @author enoyao
  */
 
-const vm = require('vm');
-const path = require('path');
-const fs = require('fs');
+const vm = require("vm");
+const path = require("path");
+const fs = require("fs");
+
 
 const testPath = process.argv.slice(2)[0];
 const code = fs.readFileSync(path.join(process.cwd(), testPath)).toString();
 
 const dispatch = event => {
-    const { fn, type, name } = event;
+    const { fn, type, name, pass } = event;
     switch (type) {
-        case 'ADD_TEST':
+        case "ADD_TEST":
             const { testBlock } = global["STATE_SYMBOL"];
             testBlock.push({ fn, name });
             break;
-        case 'BEFORE_EACH':
+        case "BEFORE_EACH":
             const { beforeEachBlock } = global["STATE_SYMBOL"];
             beforeEachBlock.push(fn);
             break;
-        case 'BEFORE_ALL':
+        case "BEFORE_ALL":
             const { beforeAllBlock } = global["STATE_SYMBOL"];
             beforeAllBlock.push(fn);
             break;
-        case 'AFTER_EACH':
+        case "AFTER_EACH":
             const { afterEachBlock } = global["STATE_SYMBOL"];
             afterEachBlock.push(fn);
             break;
-        case 'AFTER_ALL':
+        case "AFTER_ALL":
             const { afterAllBlock } = global["STATE_SYMBOL"];
             afterAllBlock.push(fn);
+            break;
+        case "COLLECT_REPORT":
+            const { reports } = global["STATE_SYMBOL"];
+            reports.push({ name, pass });
             break;
     }
 };
@@ -43,6 +48,7 @@ const createState = () => {
         beforeAllBlock: [],
         afterEachBlock: [],
         afterAllBlock: [],
+        reports: []
     };
 };
 
@@ -59,7 +65,7 @@ const jest = {
         return mockFn;
     },
     mock(mockPath, mockExports = {}) {
-        const path = require.resolve(mockPath, { paths: ['.'] });
+        const path = require.resolve(mockPath, { paths: ["."] });
         require.cache[path] = {
             id: path,
             filename: path,
@@ -91,33 +97,44 @@ const context = {
     jest,
     expect,
     require,
-    afterAll: (fn) => dispatch({ type: 'AFTER_ALL', fn }),
-    afterEach: (fn) => dispatch({ type: 'AFTER_EACH', fn }),
-    beforeAll: (fn) => dispatch({ type: 'BEFORE_ALL', fn }),
-    beforeEach: (fn) => dispatch({ type: 'BEFORE_EACH', fn }),
-    test: (name, fn) => dispatch({ type: 'ADD_TEST', fn, name }),
+    afterAll: (fn) => dispatch({ type: "AFTER_ALL", fn }),
+    afterEach: (fn) => dispatch({ type: "AFTER_EACH", fn }),
+    beforeAll: (fn) => dispatch({ type: "BEFORE_ALL", fn }),
+    beforeEach: (fn) => dispatch({ type: "BEFORE_EACH", fn }),
+    test: (name, fn) => dispatch({ type: "ADD_TEST", fn, name }),
 };
 
-const start = new Date();
+(async () => {
+    const start = new Date();
 
-vm.createContext(context);
-vm.runInContext(code, context);
+    vm.createContext(context);
+    vm.runInContext(code, context);
 
-const { testBlock, beforeEachBlock, beforeAllBlock, afterEachBlock, afterAllBlock } = global["STATE_SYMBOL"];
+    const { testBlock, beforeEachBlock, beforeAllBlock, afterEachBlock, afterAllBlock } = global["STATE_SYMBOL"];
 
-beforeAllBlock.forEach(async (beforeAll) => await beforeAll());
-testBlock.forEach(async (item) => {
-    const { fn, name } = item;
-    try {
-        beforeEachBlock.forEach(async (beforeEach) => await beforeEach());
-        await fn.apply(this);
-        afterEachBlock.forEach(async (afterEach) => await afterEach());
-        log('\x1b[32m%s\x1b[0m', `√ ${name} passed`);
-    } catch {
-        log('\x1b[32m%s\x1b[0m', `× ${name} error`);
-    }
-});
-afterAllBlock.forEach(async (afterAll) => await afterAll());
+    await new Promise((done) => {
+        beforeAllBlock.forEach(async (beforeAll) => await beforeAll());
+        testBlock.forEach(async (item) => {
+            const { fn, name } = item;
+            try {
+                beforeEachBlock.forEach(async (beforeEach) => await beforeEach());
+                await fn.apply(this);
+                dispatch({ type: "COLLECT_REPORT", name, pass: 1 });
+                afterEachBlock.forEach(async (afterEach) => await afterEach());
+                log("\x1b[32m%s\x1b[0m", `√ ${name} passed`);
+            } catch (error) {
+                dispatch({ type: "COLLECT_REPORT", name, pass: 0 });
+                console.error(error);
+                log("\x1b[32m%s\x1b[0m", `× ${name} error`);
+            }
+        });
+        afterAllBlock.forEach(async (afterAll) => await afterAll());
+        done();
+    })
 
-const end = new Date();
-log('\x1b[32m%s\x1b[0m', `Time: ${end - start}ms`);
+    const end = new Date();
+    log("\x1b[32m%s\x1b[0m", `Time: ${end - start} ms`);
+    const { reports } = global["STATE_SYMBOL"];
+    const pass = reports.reduce((pre, next) => pre.pass + next.pass);
+    log("\x1b[32m%s\x1b[0m", `All Tests: ${pass}/${reports.length} passed`);
+})();
